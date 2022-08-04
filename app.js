2e8b0cd7c9dc69ad2ef58b405762326111932e1c
Danial-Waheed <daniyalwahied@gmail.com>
2022-08-05 00:42:42 +0500

    added a tidbit

--- app.js
+++ app.js
@@ -94,6 +94,40 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
   });
 
+  // tidbit
+  const typSpd = 70;
+  const waitTime = 500;
+  const text = ["Click on the card to expand it."];
+  var mi = 0;
+
+  function writeString(e, str, i) {
+    e.innerHTML = e.innerHTML + str[i];
+
+    if (e.innerHTML.length == str.length && mi != text.length)
+      setTimeout(slowlyDelete, waitTime, e);
+  }
+
+  function deleteString(e) {
+    e.innerHTML = e.innerHTML.substring(0, e.innerHTML.length - 1);
+
+    if (e.innerHTML.length == 0) slowlyWrite(e, text[mi++]);
+  }
+
+  function slowlyDelete(e) {
+    for (var i = 0; i < e.innerHTML.length; i++) {
+      setTimeout(deleteString, (typSpd / 2) * i, e);
+    }
+  }
+
+  function slowlyWrite(e, str) {
+    for (var i = 0; i < str.length; i++) {
+      setTimeout(writeString, typSpd * i, e, str, i);
+    }
+  }
+
+  const msg = document.querySelector(".msg-icn");
+  slowlyDelete(msg);
+
   // modal
   const modal = document.querySelector("#infomodal");
   const openModal = document.querySelector(".open-button");
